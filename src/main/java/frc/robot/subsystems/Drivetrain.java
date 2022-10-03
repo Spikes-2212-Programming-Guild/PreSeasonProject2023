0d23edf53270a632abcfef3c02be0c91c61e41cb
Programming Guild <tmp@tmp.com>
2022-10-03 19:14:25 +0300

    changed ADXRS to pigeon

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.util.PigeonWrapper;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.RobotMap;
@@ -16,7 +17,7 @@ public class Drivetrain extends TankDrivetrain {
     private final CANSparkMax right1;
     private final CANSparkMax right2;
 
-    private final ADXRS450_Gyro gyro;
+    private final PigeonWrapper pigeon;
 
     public static Drivetrain getInstance() {
         if (instance == null) {
@@ -24,25 +25,26 @@ public class Drivetrain extends TankDrivetrain {
                     new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_1, CANSparkMaxLowLevel.MotorType.kBrushless),
                     new CANSparkMax(RobotMap.CAN.DRIVETRAIN_LEFT_SPARKMAX_2, CANSparkMaxLowLevel.MotorType.kBrushless),
                     new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_1, CANSparkMaxLowLevel.MotorType.kBrushless),
-                    new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_2, CANSparkMaxLowLevel.MotorType.kBrushless)
+                    new CANSparkMax(RobotMap.CAN.DRIVETRAIN_RIGHT_SPARKMAX_2, CANSparkMaxLowLevel.MotorType.kBrushless),
+                    new PigeonWrapper(RobotMap.CAN.PIGEON_TALON)
             );
         }
         return instance;
     }
 
     private Drivetrain(String namespaceName, CANSparkMax left1, CANSparkMax left2,
-                       CANSparkMax right1, CANSparkMax right2) {
+                       CANSparkMax right1, CANSparkMax right2, PigeonWrapper pigeon) {
         super(namespaceName, new MotorControllerGroup(left1, left2), new MotorControllerGroup(right1, right2));
         this.left1 = left1;
         this.left2 = left2;
         this.right1 = right1;
         this.right2 = right2;
-        this.gyro = new ADXRS450_Gyro();
+        this.pigeon = pigeon;
     }
 
     @Override
     public void configureDashboard() {
-        rootNamespace.putNumber("gyro value", gyro::getAngle);
+        rootNamespace.putNumber("pigeon yaw", pigeon::getYaw);
         rootNamespace.putNumber("left neo 1 encoder value", left1.getEncoder()::getPosition);
         rootNamespace.putNumber("left neo 2 encoder value", left2.getEncoder()::getPosition);
         rootNamespace.putNumber("right neo 1 encoder value", right1.getEncoder()::getPosition);
