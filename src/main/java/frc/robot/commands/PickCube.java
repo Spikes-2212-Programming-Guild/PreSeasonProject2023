4fd80f1f0afc3e25f5d11a943df022a3129c9e74
Programming Guild <tmp@tmp.com>
2022-10-17 11:10:01 +0300

    fixed PickCube Documentation

--- src/main/java/frc/robot/commands/PickCube.java
+++ src/main/java/frc/robot/commands/PickCube.java
@@ -1,15 +1,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Gripper;
 
 /**
  * Closes the claw to pick cubes.
+ *
+ * @see Gripper
  */
 public class PickCube extends InstantCommand {
 
     public PickCube(Gripper gripper) {
-        addRequirements(gripper);
+        addRequirements((Subsystem) gripper);
         gripper.closeSolenoid();
     }
 }
