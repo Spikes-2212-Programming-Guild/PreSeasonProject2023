ecc7f454198c56b8d1d97f990cb8660277e2c03d
גיא סביקה <guysabika@gmail.com>
2022-10-20 21:18:13 +0300

    change the location of the values

--- src/main/java/frc/robot/command/DriveToTable.java
+++ src/main/java/frc/robot/command/DriveToTable.java
@@ -9,11 +9,12 @@ import java.util.function.Supplier;
 public class DriveToTable extends DriveTankWithPID {
 
     private static RootNamespace namespace = new RootNamespace("drive to table");
-    private static final Supplier<Double> LeftSetpoint
-            = namespace.addConstantDouble("left setpoint", 0);
+
     private static final Supplier<Double> LeftSource = namespace.addConstantDouble("left source", 0);
     private static final Supplier<Double> RightSource =
             namespace.addConstantDouble("right source", 0);
+    private static final Supplier<Double> LeftSetpoint
+            = namespace.addConstantDouble("left setpoint", 0);
     private static final Supplier<Double> RightSetpoint
             = namespace.addConstantDouble("right setpoint", 0);
 
