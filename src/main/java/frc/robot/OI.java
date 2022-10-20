66f34ec9c17491e68701df4ace3f785f2374d800
גיא סביקה <guysabika@gmail.com>
2022-10-20 19:32:45 +0300

    change the OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,13 +1,20 @@
 package frc.robot;
 
+import com.spikes2212.util.XboxControllerWrapper;
 import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.command.DriveToTable;
+import frc.robot.subsystems.Drivetrain;
 
 public class OI /*GEVALD*/ {
 
     private final Joystick left = new Joystick(0);
     private final Joystick right = new Joystick(1);
-
+    private final XboxControllerWrapper xbox = new XboxControllerWrapper(2);
     public OI() {
+        Drivetrain drivetrain = Drivetrain.getInstance();
+
+        xbox.getButtonStart().whenPressed(new DriveToTable(drivetrain));
+
     }
 
     public double getLeftX() {
