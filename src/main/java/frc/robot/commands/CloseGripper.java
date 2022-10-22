e8f01b712e9438094ce8e9a139991853b0c2bfae
Programming Guild <tmp@tmp.com>
2022-10-22 09:27:02 +0300

    fixed CloseGripper documentation

--- src/main/java/frc/robot/commands/CloseGripper.java
+++ src/main/java/frc/robot/commands/CloseGripper.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Gripper;
 
 /**
- * Closes the gripper to pick cubes.
+ * Closes the gripper to pick up cubes.
  *
  * @see Gripper
  */
